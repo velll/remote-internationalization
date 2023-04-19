e63893e1aca6fc2f9f2fff382c2b0d4494e592d2
Pavel Grachev <paul@paulgoesdeep.com>
2023-04-19 19:20:13 +0200

    Cleanup tmp dir

--- examples/sinatra/app.rb
+++ examples/sinatra/app.rb
@@ -4,21 +4,14 @@ require 'remote_internationalization'
 
 require_relative './initializers/locale'
 
-puts "hello : #{RemoteInternationalization.t(:hello)}"
-
-RemoteInternationalization.with_locale(:de) do
-  puts 'DE:'
-  puts "hello : #{RemoteInternationalization.t(:hello)}"
-end
-
-get '/:locale?' do
-  RemoteInternationalization.with_locale(params['locale'] || RemoteInternationalization.default_locale) do
-    erb :index, locals: { current_locale: RemoteInternationalization.locale }
+get '/' do
+  RI.with_locale(params['locale'] || RI.default_locale) do
+    erb :index, locals: { current_locale: RI.locale }
   end
 end
 
 helpers do
   def t(...)
-    RemoteInternationalization.t(...)
+    RI.t(...)
   end
 end
